30f026c170075543f6a7a2bbd038d976a07bac7e
hnagd1 <hudsongraylaw@gmail.com>
2024-11-24 16:51:17 -0800

    commented on the trigger test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotTeleOpTriggerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robotTeleOpTriggerTest.java
@@ -13,9 +13,9 @@ public class robotTeleOpTriggerTest extends OpMode {
     private DcMotor Motor2;
     private DcMotor Motor3;
 
-    private double lx;
-    private double ly;
-    private double rx;
+    private double lx; //creates variable lx to store the value of the input on the left stick's horizontal axis
+    private double ly; //creates variable ly to store the value of the input on the left stick's vertical axis
+    private double rx; //creates variable rx to store the value of the input on the right stick's horizontal axis
 
     private double turboTrigger;
 
@@ -29,19 +29,19 @@ public class robotTeleOpTriggerTest extends OpMode {
 
         Motor1.setDirection(DcMotorSimple.Direction.REVERSE);
         Motor2.setDirection(DcMotorSimple.Direction.REVERSE);
-    }
+    } // gets the hardware map sets up motor direction for the mecanum wheels
 
     @Override
     public void loop() {
-        lx = gamepad1.left_stick_x * 1.1; //Strafe variable
-        rx = -gamepad1.left_stick_y; //Forward and backward
-        ly = -gamepad1.right_stick_x; //Turn right and left
-        turboTrigger = 1 - gamepad1.right_trigger;
+        lx = gamepad1.left_stick_x * 1.1; //Multiplies the input from the horizontal left stick on gamepad 1 by the strafe variable to account for wheel-y-ness
+        rx = -gamepad1.left_stick_y; //Uses the left stick's vertical input to move forward and backward
+        ly = -gamepad1.right_stick_x; //Uses the right stick's horizontal input to turn right and left
+        turboTrigger = 1 - gamepad1.right_trigger; //Sets turboTrigger to the opposite of the input from the trigger so that all the way down is no power instead of full power
 
         Motor0.setPower(turboTrigger * (ly + rx + lx));
         Motor1.setPower(turboTrigger * (-ly + (rx - lx)));
         Motor2.setPower(turboTrigger * (-ly + rx + lx));
         Motor3.setPower(turboTrigger * (ly + (rx - lx)));
-
+        //Sets powers of the motors normally for mecanum wheels but multiplies it by turboTrigger so the input from the trigger effects(affects?) the speed of the robot
     }
 }
