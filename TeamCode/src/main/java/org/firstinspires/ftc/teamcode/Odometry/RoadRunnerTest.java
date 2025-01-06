d116f078d235238fabfff45a4853da1ae3bce788
hnagd1 <hudsongraylaw@gmail.com>
2025-01-05 19:30:27 -0800

    First steps into roadrunner. This includes a new autonomous code using road runner. It has yet to implement driving, but it shows progress.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry/RoadRunnerTest.java
@@ -0,0 +1,83 @@
+package org.firstinspires.ftc.teamcode.Odometry;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@Config
+@Autonomous
+public class RoadRunnerTest extends LinearOpMode {
+    public class ls {
+        private DcMotor ls0;
+        private DcMotor ls1;
+
+        public ls(HardwareMap hardwareMap) {
+            ls0 = hardwareMap.get(DcMotor.class, "Motor5");
+            ls1 = hardwareMap.get(DcMotor.class, "Motor6");
+            ls0.setDirection(DcMotorSimple.Direction.FORWARD);
+            ls1.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
+
+        public class retract implements Action{
+            public boolean run(@NonNull TelemetryPacket packet) {
+                ls0.setTargetPosition(0);
+                ls1.setTargetPosition(0);
+                if (ls0.getCurrentPosition() < 10) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+
+        public Action retract(){
+            return new retract();
+        }
+
+        public class extend implements Action{
+            public boolean run(@NonNull TelemetryPacket packet) {
+                ls0.setTargetPosition(3100);
+                ls1.setTargetPosition(3100);
+                if (ls0.getCurrentPosition() > 3050) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+
+        public Action extend(){
+            return new extend();
+        }
+    }
+
+
+
+    @Override
+    public void runOpMode() {
+        ls ls = new ls(hardwareMap);
+
+        //TODO: Figure out stop code here
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        ls.retract(),
+                        ls.extend(),
+                        ls.retract(),
+                        ls.extend(),
+                        ls.retract(),
+                        ls.extend(),
+                        ls.retract(),
+                        ls.extend()
+        ));
+    }
+}
